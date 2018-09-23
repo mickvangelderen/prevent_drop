59791ffba627ef4124dc51cd4fb6a8f135c5983d
Mick van Gelderen <mickvangelderen@gmail.com>
2018-09-23 17:58:17 +0200

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-//! A macro to prevent a type from dropping.
+//! A macro to prevent value of a type from being implicitly dropped.
 //!
 //! At the time of writing the only way to ensure some cleanup code is
-//! run, is by implementing Drop. Drop has some limitations however: you
+//! run is by implementing Drop. Drop has some limitations however: you
 //! can't pass extra arguments or report errors from it. Even the
 //! standard library faces this problem:
 //!
@@ -18,10 +18,14 @@
 //! }
 //! ```
 //!
-//! This macro allows you to detect if a value would be dropped at
-//! compile time if and only if the compiler optimization elides the
-//! drop function call. This means that you have to enable optimizations
-//! for it to work.
+//! Something called linear types would help, see [this
+//! thread](https://users.rust-lang.org/t/prevent-drop-at-compile-time/20508)
+//! for more information, but that is not available as of September
+//! 2018. For now the `prevent_drop!` macro allows you to detect if a
+//! value would be dropped at compile time if and only if the compiler
+//! optimization elides the drop function call. This means that you have
+//! to enable optimizations for it to work.
+//!
 //!
 //! ## Example
 //!
@@ -75,10 +79,23 @@
 //!
 //! By default, `prevent_drop` only works when optimizations are
 //! enabled. The macro relies on optimizations to remove the drop
-//! function if it isn't called. If you for some reason need to test
-//! your code without optimizations, you can enable the `abort` feature.
-//! This will make drop abort the process in debug builds. A debugger
-//! can help you find out why.
+//! function if it isn't called. To enable optimizations for debug
+//! builds and tests you can use the following. Perhaps you need a
+//! `opt-level = 2` before the compiler elides drop calls.
+//!
+//! ```
+//! [profile.dev]
+//! opt-level = 1
+//!
+//! [profile.test]
+//! opt-level = 1
+//! ```
+//!
+//! Alternatively, you can enable the `abort` feature. This will make
+//! `prevent_drop!` abort the process at run time instead of yielding a
+//! compile error. This can be useful to figure out where drop is being
+//! called through debugging. It requires you to find a case where this
+//! code actually executes though!
 
 #![doc(html_root_url = "https://docs.rs/prevent_drop")]
 #![deny(missing_docs)]
